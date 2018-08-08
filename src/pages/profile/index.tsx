aef9fa4ecb0ee23b32fd60f045b53b3d2ba257b4
ibleedfilm <rem@lansangan.com>
2018-08-08 13:56:28 +0800

    Init profile page :sparkles:

---
+++ src/pages/profile/index.tsx
@@ -0,0 +1,14 @@
+const Profile = () => (
+  <div>
+    <h1>Profile Page</h1>
+    <h3>Rem Lampa</h3>
+
+    <h3>Open Source Contributions</h3>
+    <ul>
+      <li>Project 1</li>
+      <li>Project 3</li>
+    </ul>
+  </div>
+);
+
+export default Profile;
