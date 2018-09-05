b35886cf0a355fdf9b16c7eafb68105a979bb2f3
ibleedfilm <rem@lansangan.com>
2018-09-05 11:39:47 +0800

    Remove CRUD REST API endpoints :fire:

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,12 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const next = require('next');
 
 require('isomorphic-unfetch');
 
 // const github = require('octonode');
-
-const db = require('../mock-db/db.json');
 
 require('dotenv').config();
 
@@ -22,69 +21,6 @@ app
     const server = express();
 
     server.use(bodyParser.json());
-
-    server.post('/get-user-repos', (_, res) => {
-      console.log('from server:');
-
-      const { username, repositories } = db;
-
-      res.send({ username, repositories });
-    });
-
-    server.post('/add-user-repo', async (req, res) => {
-      console.log('from server:');
-
-      const { repoOwner, repoName } = req.body;
-
-      const githubReqURL = `https://api.github.com/repos/${repoOwner}/${repoName}/commits?author=RemLampa`;
-
-      const githubRes = await fetch(githubReqURL, {
-        method: 'GET',
-      });
-
-      if (githubRes.status !== 200) {
-        const errorMessage = `${repoOwner}/${repoName} is an invalid Github repository.`;
-
-        return res.status(404).json({ error: { message: errorMessage } });
-      }
-
-      const commits = await githubRes.json();
-
-      if (!commits.length) {
-        const errorMessage = `You are not a contributor in ${repoOwner}/${repoName}`;
-
-        return res.status(200).json({ error: { message: errorMessage } });
-      }
-
-      db.repositories.push({ repoOwner, repoName });
-
-      const successMessage = `${repoOwner}/${repoName} has been successfully added to user's portfolio`;
-
-      return res.status(200).json({ message: successMessage });
-    });
-
-    server.post('/delete-user-repo', (req, res) => {
-      const { id } = req.body;
-
-      const { repositories } = db;
-
-      if (id < 0 || id > repositories.length - 1) {
-        const error = {
-          message: 'Invalid ID!',
-        };
-
-        return res.status(416).json({ error });
-      }
-
-      repositories.splice(id, 1);
-
-      const successResponse = {
-        id,
-        message: `Repo ID ${id} was succesfully deleted.`,
-      };
-
-      return res.status(200).json(successResponse);
-    });
 
     server.get('/callback', async (req, res) => {
       const { code } = req.query;
